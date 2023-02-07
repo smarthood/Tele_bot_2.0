0def24a2425a0498e23082bc78d44d5fbf90f0ae
smarthood <renolddickson18@gmail.com>
2023-02-07 14:34:53 +0530

    key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const birthdays = require('./birthdays.json');
 const holidays = require('./holidays.json');
-const bot = new TelegramBot(process.env.token, { polling: { interval: 2000 } });
+const token="5921202727:AAF_c7OE8W_TjJf1r_srYAn0au_Dz8CJrB0"
+const bot = new TelegramBot(token, { polling: { interval: 2000 } });
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const userName= msg.chat.last_name;
